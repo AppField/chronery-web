5b6cb37530c96e82ba45cd666e5e13ec8590cd19
WebField <roman@muehlfeldner.net>
2017-03-28 15:41:04 +0200

    Fixed layout

--- src/app/components/sidenav-daypicker/sidenav-daypicker.component.ts
+++ src/app/components/sidenav-daypicker/sidenav-daypicker.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'wtc-sidenav-daypicker',
-  templateUrl: './sidenav-daypicker.component.html',
-  styleUrls: ['./sidenav-daypicker.component.scss']
+	selector: 'wtc-sidenav-daypicker',
+	templateUrl: './sidenav-daypicker.component.html',
+	styleUrls: ['./sidenav-daypicker.component.scss']
 })
 export class SidenavDaypickerComponent implements OnInit {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
