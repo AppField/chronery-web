172f0ff5cc95f08ad52592c95193a0ed63f8d320
WebField <roman@muehlfeldner.net>
2017-04-04 21:49:56 +0200

    Generated basic pages for routes

---
+++ src/app/pages/hours-of-work/hours-of-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wtc-hours-of-work',
+  templateUrl: './hours-of-work.component.html',
+  styleUrls: ['./hours-of-work.component.scss']
+})
+export class HoursOfWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
