5c0beefd5546f2ca41a5799dadebaeebaa586d67
WebField <roman@muehlfeldner.net>
2017-07-23 00:44:24 +0200

    Dashboard: added todays date and routerLink to open the todays date in working hours.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Utility} from '../../utils/utility';
 
 @Component({
-  selector: 'chy-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+	selector: 'chy-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+	todaysDate = new Date();
+	todaysLink: string;
 
-  constructor() { }
+	constructor() {
+		this.todaysLink = '/working-hours/' + Utility.encodeDate(this.todaysDate);
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
