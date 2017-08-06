aba0f6d0d646b5e653e5b13b9217db20efd27c21
WebField <roman@muehlfeldner.net>
2017-08-06 09:12:35 +0200

    Dashboard: added buttons to navigate between months (chart)

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -13,7 +13,9 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 	todaysDate = new Date();
+	chartMonth = new Date();
 	todaysLink: string;
+	chartLink: string;
 	dataFound = true;
 	chartData: Work[];
 	// TODO: remove this workaround. Necessary as the async time causes an error whenn building the prod package.
@@ -21,9 +23,27 @@ export class DashboardComponent implements OnInit {
 
 	constructor(private router: Router, private workingHoursDb: WorkingHoursDbService) {
 		this.todaysLink = '/working-hours/' + Utility.encodeDate(this.todaysDate);
+		this.updateChart();
+	}
 
-		const startDate = Utility.encodeDate(moment().startOf('month').toDate());
-		const endDate = Utility.encodeDate(moment().endOf('month').toDate());
+	ngOnInit() {
+	}
+
+	nextMonth(): void {
+		this.chartMonth = moment(this.chartMonth).add(1, 'month').date(1).toDate();
+		this.updateChart();
+	}
+
+	previousMonth(): void {
+		this.chartMonth = moment(this.chartMonth).subtract(1, 'month').date(1).toDate();
+		this.updateChart();
+	}
+
+	updateChart(): void {
+		this.chartLink = '/working-hours/' + Utility.encodeDate(this.chartMonth);
+
+		const startDate = Utility.encodeDate(moment(this.chartMonth).startOf('month').toDate());
+		const endDate = Utility.encodeDate(moment(this.chartMonth).endOf('month').toDate());
 
 		const filter = new WorkingHoursFilter();
 		filter.date = startDate;
@@ -32,14 +52,14 @@ export class DashboardComponent implements OnInit {
 			this.chartData = data;
 			(data.length > 0) ? this.dataFound = true : this.dataFound = false;
 		});
-
-	}
-
-	ngOnInit() {
 	}
 
 	navigateToWorkingHours(): void {
 		this.router.navigate([this.todaysLink]);
+	}
+
+	navigateToChartMonth(): void {
+		this.router.navigate([this.chartLink]);
 	}
 
 	navigateToProjects(): void {
