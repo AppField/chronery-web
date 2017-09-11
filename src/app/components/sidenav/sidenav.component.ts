b744ba7030017c8364e3793379467d7269b2cf6b
WebField <roman@muehlfeldner.net>
2017-09-11 15:13:46 +0200

    Sidenav component: fixed the sidenav-containers width when the sidenav widths gets changed. Due to bug in angular material 2.

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
 import { Utility } from '../../utils/utility';
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+} from '@angular/animations';
 
 interface RouterItem {
 	link: string;
@@ -12,10 +19,37 @@ interface RouterItem {
 	selector: 'chy-sidenav',
 	templateUrl: './sidenav.component.html',
 	styleUrls: ['./sidenav.component.scss'],
+	animations: [
+		trigger('sidenavState', [
+			state('expandedState', style({
+				width: '*'
+			})),
+			state('collapsedState', style({
+				width: '56px'
+			})),
+			transition('expandedState => collapsedState', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)')),
+			transition('collapsedState => expandedState', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+		]),
+		trigger('labelState', [
+			state('expandedState', style({
+				opacity: 1,
+				visibility: 'visible',
+			})),
+			state('collapsedState', style({
+				opacity: 0,
+				visibility: 'hidden'
+			})),
+			transition('expandedState => collapsedState', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)')),
+			transition('collapsedState => expandedState', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+		])
+	]
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, AfterViewInit {
 	routerItems: RouterItem[];
 	collapsed = true;
+	state = 'expandedState';
+
+	@ViewChild('sidenavContainer') sidenav;
 
 	constructor(private media: ObservableMedia,
 				private detector: ChangeDetectorRef) {
@@ -53,22 +87,34 @@ export class SidenavComponent implements OnInit {
 	ngOnInit() {
 		if (this.media.isActive('xs')) {
 			this.collapsed = true;
+			this.state = 'collapsedState';
 		}
+	}
+
+	ngAfterViewInit() {
+		// TODO: Remove this workaround as it is a bug in angular material 2 beta 10
+		// Necessary to recalculate sidenav-containers width when the sidenav width gets changed.
+		this.sidenav._ngZone.onMicrotaskEmpty.subscribe(() => {
+			setTimeout(() => {
+				this.sidenav._updateStyles();
+				this.sidenav._changeDetectorRef.markForCheck();
+			});
+		});
 	}
 
 	get isMobile(): boolean {
 		return this.media.isActive('xs');
 	}
 
-
 	toggleCollapse(): void {
 		this.collapsed = !this.collapsed;
+		this.state = (this.state === 'expandedState') ? 'collapsedState' : 'expandedState';
 	}
 
 	recollapseNav(): void {
 		if (this.isMobile) {
 			this.collapsed = true;
-			this.detector.detectChanges();
+			this.state = 'collapsedState';
 		}
 	}
 }
