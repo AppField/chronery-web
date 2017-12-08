bcaf62ca8cfd2bf6cde1be8de23679e0d4027b3f
WebField <roman@muehlfeldner.net>
2017-12-08 18:03:52 +0100

    Added console outputs to authGuard.

--- src/app/user/auth-guard.service.ts
+++ src/app/user/auth-guard.service.ts
@@ -1,6 +1,5 @@
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -12,11 +11,13 @@ export class AuthGuard implements CanActivate {
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
 		return new Promise<boolean>((resolve, reject) => {
 			this.authService.isAuthenticated().subscribe(isAuthenticated => {
-
+				console.log('CHECKING AUTHENTICATION');
 				if (isAuthenticated) {
 					console.log('Authenticated!');
 					resolve(true);
 				} else {
+					console.error('Not Authenticated!');
+					resolve(false);
 					this.router.navigate(['login']);
 				}
 				resolve(false);
