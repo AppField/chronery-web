cff2017d2567ec35f698ebca9ec3e9019ee63979
= <roman@muehlfeldner.net>
2018-02-21 09:52:02 +0100

    Implemented MatMomentDateAdapter

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import {
+    DateAdapter,
+    MAT_DATE_FORMATS,
+    MAT_DATE_LOCALE,
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
@@ -24,6 +27,7 @@ import {
     MatToolbarModule,
     MatTooltipModule
 } from '@angular/material';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from "@angular/material-moment-adapter";
 
 @NgModule({
     imports: [
@@ -49,7 +53,7 @@ import {
         MatCheckboxModule,
         MatSelectModule,
         MatSortModule,
-        MatPaginatorModule
+        MatPaginatorModule,
     ],
     exports: [
         MatButtonModule,
@@ -75,7 +79,14 @@ import {
         MatSelectModule,
         MatSortModule,
         MatPaginatorModule
-    ]
+    ],
+    providers: [
+        // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+        // `MatMomentDateModule` in your applications root module. We provide it at the component level
+        // here, due to limitations of our example generation script.
+        { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    ],
 })
 
 export class MaterialModule {
